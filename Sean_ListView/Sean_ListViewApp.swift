b4189e55df8e03ef36bee3bf826ae80eba0e7564
MohanedAzez <mohanedaliraqe90@gmail.com>
2024-01-31 19:04:39 +0300

    Initial Commit

---
+++ Sean_ListView/Sean_ListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sean_ListViewApp.swift
+//  Sean_ListView
+//
+//  Created by Mohanned on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Sean_ListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
